c7bc683ebbd489403e5563b3bd48729ab7892b97
jeongha-daisy <jeongha.daisy@gmail.com>
2025-05-22 17:34:12 +0900

    feat: implement game state

--- game_v1.py
+++ game_v1.py
@@ -2,7 +2,7 @@
 import pygame
 import random
 
-import smbus
+# import smbus
 import time
 
 pygame.init()
@@ -27,7 +27,7 @@ arrow_list = []
 address = 0x48
 A0 = 0x40
 A1 = 0x41
-bus = smbus.SMBus(1)
+# bus = smbus.SMBus(1)
 
 
 # 중앙에서 시작하는 플레이어
@@ -40,129 +40,148 @@ arrow_image = pygame.transform.scale(arrow_image, (arrow_size, arrow_size))
 player_image = pygame.image.load("assets/dave_front.png").convert_alpha()
 player_image = pygame.transform.scale(player_image, (player_size, player_size))
 
+# 폰트 설정하기
+textFont = pygame.font.SysFont(None, 50)
+
+# 게임 상태
+game_state = 0
+
 while running:
     # 종료 조건
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
     # 배경 색
     screen.fill("#c9c9c9")
 
-    # ========
-    # 화살 그리기
-    for arrow in arrow_list:
-        # 바닥에 닿았으면 리스트에서 제거하기
-        if arrow[2] == 0 and arrow[0].y > height:
-            arrow_list.remove(arrow)
-        if arrow[2] == 1 and arrow[0].x < 0:
-            arrow_list.remove(arrow)
-        if arrow[2] == 2 and arrow[0].y < 0:
-            arrow_list.remove(arrow)
-        if arrow[2] == 3 and arrow[0].x > width:
-            arrow_list.remove(arrow)
+    if game_state == 0:
+        startText = textFont.render("PRESS K TO START", True, (255, 255, 255))
+        text_rect = startText.get_rect(center=(width/2, height/2))
+        screen.blit(startText, text_rect)
 
-    # 10개 이하라면 또 추가
-    if len(arrow_list) < limited_arrows:
-        # 랜덤한 위치 속도, 시작 위치(북, 동, 남, 서)를 arrow 리스트에 넣는다.
-        direction = random.randint(0, 4)
-        # 북쪽에서 날아오는 화살
-        if direction == 0:
-            arrowX = random.randrange(0, width)
-            speed = random.randrange(arrow_min_speed, arrow_max_speed)
-            arrow_list.append([pygame.Vector2(arrowX, 0), speed, direction])
-        # 동쪽에서 날아오는 화살
-        elif direction == 1:
-            arrowY = random.randrange(0, height)
-            speed = random.randrange(arrow_min_speed, arrow_max_speed)
-            arrow_list.append([pygame.Vector2(width, arrowY), speed, direction])
-        # 남쪽에서 날아오는 화살
-        elif direction == 2:
-            arrowX = random.randrange(0, width)
-            speed = random.randrange(arrow_min_speed, arrow_max_speed)
-            arrow_list.append([pygame.Vector2(arrowX, height), speed, direction])
-        # 서쪽에서 날아오는 화살
-        else:
-            arrowY = random.randrange(0, height)
-            speed = random.randrange(arrow_min_speed, arrow_max_speed)
-            arrow_list.append([pygame.Vector2(0, arrowY), speed, direction])
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_k]:
+           game_state = 1
 
-    
-    # 화살 그리기 및 이동
-    for arrow in arrow_list:
-        screen.blit(arrow_image, arrow[0])
-        if arrow[2] == 0:
-            arrow[0].y += arrow[1] * dt
-        elif arrow[2] == 1:
-            arrow[0].x -= arrow[1] * dt
-        elif arrow[2] == 2:
-            arrow[0].y -= arrow[1] * dt
-        else:
-            arrow[0].x += arrow[1] * dt
-    # ========
+    elif game_state == 1:
+        # ========
+        # 화살 그리기
+        for arrow in arrow_list:
+            # 바닥에 닿았으면 리스트에서 제거하기
+            if arrow[2] == 0 and arrow[0].y > height:
+                arrow_list.remove(arrow)
+            if arrow[2] == 1 and arrow[0].x < 0:
+                arrow_list.remove(arrow)
+            if arrow[2] == 2 and arrow[0].y < 0:
+                arrow_list.remove(arrow)
+            if arrow[2] == 3 and arrow[0].x > width:
+                arrow_list.remove(arrow)
 
-    # 플레이어 그리기
-    screen.blit(player_image, player_pos)
+        # 10개 이하라면 또 추가
+        if len(arrow_list) < limited_arrows:
+            # 랜덤한 위치 속도, 시작 위치(북, 동, 남, 서)를 arrow 리스트에 넣는다.
+            direction = random.randint(0, 4)
+            # 북쪽에서 날아오는 화살
+            if direction == 0:
+                arrowX = random.randrange(0, width)
+                speed = random.randrange(arrow_min_speed, arrow_max_speed)
+                arrow_list.append([pygame.Vector2(arrowX, 0), speed, direction])
+            # 동쪽에서 날아오는 화살
+            elif direction == 1:
+                arrowY = random.randrange(0, height)
+                speed = random.randrange(arrow_min_speed, arrow_max_speed)
+                arrow_list.append([pygame.Vector2(width, arrowY), speed, direction])
+            # 남쪽에서 날아오는 화살
+            elif direction == 2:
+                arrowX = random.randrange(0, width)
+                speed = random.randrange(arrow_min_speed, arrow_max_speed)
+                arrow_list.append([pygame.Vector2(arrowX, height), speed, direction])
+            # 서쪽에서 날아오는 화살
+            else:
+                arrowY = random.randrange(0, height)
+                speed = random.randrange(arrow_min_speed, arrow_max_speed)
+                arrow_list.append([pygame.Vector2(0, arrowY), speed, direction])
 
-    # keyboard moving
+        # 화살 그리기 및 이동
+        for arrow in arrow_list:
+            screen.blit(arrow_image, arrow[0])
+            if arrow[2] == 0:
+                arrow[0].y += arrow[1] * dt
+            elif arrow[2] == 1:
+                arrow[0].x -= arrow[1] * dt
+            elif arrow[2] == 2:
+                arrow[0].y -= arrow[1] * dt
+            else:
+                arrow[0].x += arrow[1] * dt
+        # ========
+
+        # 플레이어 그리기
+        screen.blit(player_image, player_pos)
+
+        # keyboard moving
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and player_pos.y > 0:
+            player_pos.y -= 300 * dt
+        if keys[pygame.K_s] and player_pos.y < height:
+            player_pos.y += 300 * dt
+        if keys[pygame.K_a] and player_pos.x > 0:
+            player_pos.x -= 300 * dt
+        if keys[pygame.K_d] and player_pos.x < width:
+            player_pos.x += 300 * dt
+
+        """
+        # joystcik moving
+        bus.write_byte(address, A0)
+        time.sleep(0.01)
+        value1 = bus.read_byte(address)
+
+        bus.write_byte(address, A1)
+        time.sleep(0.01)
+        value2 = bus.read_byte(address)
+
+        print(value1, value2)
+        # down
+        if value1 < 100 and player_pos.x > (0 - player_size / 2):
+            print("left")
+            player_pos.x -= player_speed * dt
+        if value1 > 220 and player_pos.x < (width - player_size / 2):
+            print("right")
+            player_pos.x += player_speed * dt
+        if value2 < 100 and player_pos.y > (0 - player_size / 2):
+            print("up")
+            player_pos.y -= player_speed * dt
+        if value2 > 220 and player_pos.y < (height - player_size / 2):
+            print("down")
+            player_pos.y += player_speed * dt
     """
-    # 움직임 제어
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w] and player_pos.y > 0:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s] and player_pos.y < height:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a] and player_pos.x > 0:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d] and player_pos.x < width:
-        player_pos.x += 300 * dt
 
-    """
-    # joystcik moving
-    bus.write_byte(address, A0)
-    time.sleep(0.01)
-    value1 = bus.read_byte(address)
-    
-    bus.write_byte(address, A1)
-    time.sleep(0.01)
-    value2 = bus.read_byte(address)
-    
-    print(value1, value2)
-    # down
-    if value1 < 100 and player_pos.x > (0 - player_size / 2):
-        print("left")
-        player_pos.x -= player_speed * dt
-    if value1 > 220 and player_pos.x < (width - player_size / 2):
-        print("right")
-        player_pos.x += player_speed * dt
-    if value2 < 100 and player_pos.y > (0 - player_size / 2):
-        print("up")
-        player_pos.y -= player_speed * dt
-    if value2 > 220 and player_pos.y < (height - player_size / 2):
-        print("down")
-        player_pos.y += player_speed * dt
 
-    
-    
-    # 다시 그리기
-    pygame.display.flip()
+        # ========
+        # 닿았는지 검사
+        # 플레이어 콜라이더 값
+        left_value = player_pos.x - player_size / 2
+        right_value = player_pos.x + player_size / 2
+        top_value = player_pos.x - player_size / 2
+        bottom_value = player_pos.x + player_size / 2
 
-    # ========
-    # 닿았는지 검사
-    # 플레이어 콜라이더 값
-    left_value = player_pos.x - player_size / 2
-    right_value = player_pos.x + player_size / 2
-    top_value = player_pos.x - player_size / 2
-    bottom_value = player_pos.x + player_size / 2
+        for arrow in arrow_list:
+            # 화살의 좌표가 플레이어 콜라이더 안에 들어왔으면
+            if arrow[0].x > left_value and arrow[0].x < right_value and arrow[0].y > top_value and arrow[
+                0].y < bottom_value:
+                game_state = 2
 
-    for arrow in arrow_list:
-        # 화살의 좌표가 플레이어 콜라이더 안에 들어왔으면
-        if arrow[0].x > left_value and arrow[0].x < right_value and arrow[0].y > top_value and arrow[0].y < bottom_value:
-            print("게임 종료")
+        # ========
+    elif game_state == 2:
+        stopText = textFont.render("GAME OVER", True, (255, 255, 255))
+        text_rect = stopText.get_rect(center=(width / 2, height / 2))
+        screen.blit(stopText, text_rect)
+    else:
+        pass
 
-    # ========
 
     # 프레임 속도
     dt = clock.tick(60) / 1000
+    # 다시 그리기
+    pygame.display.flip()
 
 pygame.quit()
